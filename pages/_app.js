6d2806349451bf4399edbddfab729baf449c9796
Harsh Bajpai <bajpaiharsh244@gmail.com>
2021-05-26 17:37:30 +0530

    chore: add metadata for facebook verification

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'theme-ui'
 import theme from '../lib/theme/index'
+import Head from 'next/head'
 
 import '../lib/css/index.css'
 import 'react-toastify/dist/ReactToastify.css'
@@ -8,8 +9,11 @@ import { ToastContainer } from 'react-toastify'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <ToastContainer />
+    <ThemeProvider theme={theme}>   
+        <Head>
+          <meta name="facebook-domain-verification" content="uwn7li3powk0yic8r9jfyy9pihtgxn" />
+        </Head>     
+       <ToastContainer />
       <Component {...pageProps} />
     </ThemeProvider>
   )
